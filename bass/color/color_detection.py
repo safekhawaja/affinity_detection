55305ffbbc123a3cce291ae50a8f6f8d80d40986
Saif <68385788+safekhawaja@users.noreply.github.com>
2020-09-01 11:46:26 -0400

    new hsv for striped bass

--- bass/color/color_detection.py
+++ bass/color/color_detection.py
@@ -8,30 +8,26 @@ cap.set(3, frameWidth)
 cap.set(4, frameHeight)
 cap.set(10, 150)
 
-myColors = [[99, 134, 0, 123, 255, 255],
-            [1, 137, 151, 14, 255, 255],
-            [61, 102, 42, 93, 255, 223]]
+myColors = [18, 46, 72, 40, 159, 238]
 
-myColorValues = [[255, 0, 0],
-                 [0, 0, 255],
-                 [0, 255, 0]]
+myColorValues = [[255, 198, 145]]
 
-myPoints = [] #[x, y, colorID]
+myPoints = []  # [x, y, colorID]
 
 
 def findColor(img, myColors, myColorValues):
     imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     count = 0
-    newPoints=[]
+    newPoints = []
     for color in myColors:
         lower = np.array(color[0:3])
         upper = np.array(color[3:6])
         mask = cv2.inRange(imgHSV, lower, upper)
-        x,y = getContours(mask)
-        cv2.circle(imgResult, (x,y), 10, myColorValues[count],cv2.FILLED)
+        x, y = getContours(mask)
+        cv2.circle(imgResult, (x, y), 10, myColorValues[count], cv2.FILLED)
         if x != 0 and y != 0:
             newPoints.append([x, y, count])
-        count +=1
+        count += 1
         # cv2.imshow(str(color[0]), mask)
     return newPoints
 
@@ -42,15 +38,17 @@ def getContours(img):
     for cnt in contours:
         area = cv2.contourArea(cnt)
         if area > 7:
-            #cv2.drawContours(imgResult, cnt, -1, (255, 0, 0), 3)
+            # cv2.drawContours(imgResult, cnt, -1, (255, 0, 0), 3)
             peri = cv2.arcLength(cnt, True)
             approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
             x, y, w, h = cv2.boundingRect(approx)
-    return x+w//2, y
+    return x + w // 2, y
+
 
 def drawOnCanvas(myPoints, myColorValues):
     for point in myPoints:
         cv2.circle(imgResult, (point[0], point[1]), 10, myColorValues[point[2]], cv2.FILLED)
+
 
 while True:
     success, img = cap.read()
